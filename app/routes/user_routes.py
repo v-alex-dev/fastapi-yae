e715f1e556351dc8e4a310c2e468839cdee75201
dev_vens <vensalex1991@gmail.com>
2026-06-14 20:27:13 +0200

    Test authentification security for routes

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -4,11 +4,17 @@
 # They delegate everything to the controller.
 
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from app.controllers import user_controller
 from app.schemas.user_schema import UserCreate, UserUpdate, UserOut
+from app.utils.dependencies import get_current_user
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get("/me", response_model=UserOut)
+async def get_me(current_user = Depends(get_current_user)):
+    return await user_controller.get_current_user_info(current_user)
 
 @router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data: UserCreate) -> UserOut:
@@ -19,6 +25,8 @@ async def create_user(user_data: UserCreate) -> UserOut:
 async def list_users():
     """list all users"""
     return await user_controller.list_users()
+
+
 
 @router.get("/{user_id}", response_model=UserOut)
 async def read_user(user_id: int) -> UserOut:
@@ -34,3 +42,4 @@ async def update_user(user_id:int, user_data: UserUpdate) -> UserOut:
 async def delete_user(user_id: int):
     """delete a user"""
     return await user_controller.delete_user(user_id)
+
