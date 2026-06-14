1f6516d28659c4f566f1246a93a55e9c5e368127
dev_vens <vensalex1991@gmail.com>
2026-06-14 11:06:15 +0200

    include route into main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from fastapi import FastAPI
 from app.config.settings import settings
 from app.db.database import connect_to_db, disconnect_from_db
 from app.middleware.logging_middleware import LoggingMiddleware
-from app.routes import health_routes
+from app.routes import health_routes, user_routes
 
 
 @asynccontextmanager
@@ -29,3 +29,4 @@ app.add_middleware(LoggingMiddleware)
 
 # Register routers
 app.include_router(health_routes.router)
+app.include_router(user_routes.router)
