8ebff1e5ef8c84330fef1ab284a3bd0c82413ad6
dev_vens <vensalex1991@gmail.com>
2026-06-14 10:42:52 +0200

    Create routers for user & first route function:
    post -> create user

---
+++ app/routes/user_routes.py
@@ -0,0 +1,17 @@
+# app/routes/user_routes.py
+#
+# Routes only declare URL paths and HTTP methods.
+# They delegate everything to the controller.
+
+
+from fastapi import APIRouter, status
+from app.controllers import user_controller
+from app.schemas.user_schema import UserCreate, UserUpdate, UserOut
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+async def create_user(user_data: UserCreate) -> UserOut:
+    """create a new user"""
+    return await user_controller.create_user(user_data)
+
