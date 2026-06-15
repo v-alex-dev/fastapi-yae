15d7fa9d72765022c3ce8f4c1a5c2de86d028e41
dev_vens <vensalex1991@gmail.com>
2026-06-15 15:57:55 +0200

    Create & add function controller for ingredient

---
+++ app/controllers/ingredient_controller.py
@@ -0,0 +1,28 @@
+import asyncpg
+
+from fastapi import HTTPException
+from app.services import ingredient_service
+from app.schemas.ingredient_schema import IngredientUpdate, IngredientCreate, IngredientDelete, IngredientOut
+
+
+async def create_ingredient(ingredient: IngredientCreate) -> IngredientOut:
+
+    if ingredient is None:
+        raise HTTPException(status_code=400, detail="Ingredient is required")
+
+    return await ingredient_service.ingredient_create(ingredient)
+
+async def list_ingredients() -> list[asyncpg.Record]:
+    return await ingredient_service.list_ingredients()
+
+async def update_ingredient(ingredient_id: int, ingredient: IngredientUpdate) -> IngredientOut:
+    if ingredient is None:
+        raise HTTPException(status_code=400, detail="Ingredient is required")
+
+    if ingredient_id != ingredient.id:
+        raise HTTPException(status_code=404, detail="Ingredient is not found")
+
+    return await ingredient_service.ingredient_update(ingredient)
+
+async def delete_ingredient(ingredient_id: int)-> asyncpg.Record:
+    return await ingredient_service.ingredient_delete(ingredient_id)
