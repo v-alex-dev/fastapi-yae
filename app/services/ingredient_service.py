4eba65a6a257fa204f641004168933d2caee0529
dev_vens <vensalex1991@gmail.com>
2026-06-15 15:58:19 +0200

    Add update, get by id  and delete for ingredient

--- app/services/ingredient_service.py
+++ app/services/ingredient_service.py
@@ -16,4 +16,38 @@ async def ingredient_create(ingredient: IngredientCreate) -> IngredientOut:
 
     return await pool.fetchrow(query, ingredient.name)
 
+async def ingredient_update(ingredient: IngredientUpdate) -> IngredientOut:
+    """update ingredient """
+    pool = get_pool()
 
+    query = """
+        UPDATE Ingredient
+        SET name = $1,
+        WHERE id = $2,
+        RETURNING id, name, created_at;
+    """
+
+    return await pool.fetchrow(query, ingredient.name, ingredient.id)
+
+async def get_ingredient_by_id(ingredient_id: int) -> asyncpg.Record | None:
+    pool = get_pool()
+    query = """
+    SELECT id, name FROM Ingredient WHERE id = $1;
+    """
+    return await pool.fetchrow(query, ingredient_id)
+
+async def list_ingredients() -> list[asyncpg.Record]:
+    pool = get_pool()
+    query =""""
+        SELECT id, name
+        FROM Ingredient
+        ORDER BY name DESC;
+    """
+    return await pool.fetch(query)
+
+async def ingredient_delete(ingredient_id: int) -> asyncpg.Record | None:
+    pool = get_pool()
+    query = """
+    DELETE FROM Ingredient WHERE id = $1 RETURNING id;
+    """
+    return await pool.fetchrow(query, ingredient_id)
