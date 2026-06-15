3505449c0c211ddbce8ebffa87cbee6b8ddf39aa
dev_vens <vensalex1991@gmail.com>
2026-06-15 11:52:22 +0200

    Create schema Ingredient

---
+++ app/schemas/ingredient_schema.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class IngredientCreate(BaseModel):
+    name: str
+
+class IngredientUpdate(BaseModel):
+    name: str
+
+class IngredientDelete(BaseModel):
+    id: int
+
+class IngredientOut(BaseModel):
+    id: int
+    name: str
+    creation_date: datetime
