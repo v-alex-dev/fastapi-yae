dc5cf1352ff3546a2ec2b0617af87febcd83ac0d
dev_vens <vensalex1991@gmail.com>
2026-06-15 16:07:06 +0200

    Add routing for ingredient

---
+++ app/routes/ingredient_routes.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.testing.pickleable import User
+
+from app.controllers import ingredient_controller
+from app.schemas.ingredient_schema import IngredientCreate, IngredientUpdate, IngredientOut
+from app.utils.dependencies import get_current_user
+
+router = APIRouter(prefix="/ingredients", tags=["ingredients"])
+
+
+@router.post("/new", response_model=IngredientOut)
+async def ingredient_create(ingredient: IngredientCreate, current_user: User = Depends(get_current_user)):
+    return await ingredient_controller.create_ingredient(ingredient)
+
+@router.patch("/{ingredient_id}",response_model=IngredientOut)
+async def ingredient_update(ingredient: IngredientUpdate) -> IngredientOut:
+    return await ingredient_controller.update_ingredient(ingredient)
+
+@router.get("/all", response_model=list[IngredientOut])
+async def ingredient_list():
+    return await ingredient_controller.list_ingredients()
+
+@router.delete("/{ingredient_id}",response_model=IngredientOut)
+async def ingredient_delete(ingredient_id: int):
+    return await ingredient_controller.delete_ingredient(ingredient_id)
