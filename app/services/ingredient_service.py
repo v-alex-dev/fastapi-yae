54de1209e48633b990539ee41f750551e7e39269
dev_vens <vensalex1991@gmail.com>
2026-06-16 12:01:52 +0200

    Fix & add ingredient create function

--- app/services/ingredient_service.py
+++ app/services/ingredient_service.py
@@ -1,20 +1,24 @@
 import asyncpg
 
 from app.db.database import get_pool
-from app.schemas.ingredient_schema import IngredientCreate, IngredientUpdate, IngredientDelete, IngredientOut
+from app.schemas.ingredient_schema import IngredientCreate, IngredientUpdate, IngredientOut
 
-async def ingredient_create(ingredient: IngredientCreate) -> IngredientOut:
-    """create ingredient """
+async def ingredient_create(name: str, user_id: int) -> IngredientOut:
+    """
+        Insert a new ingredient.
+        user_id is None for global ingredients, or the id of the user
+        who created a personal ingredient.
+    """
 
     pool =  get_pool()
 
     query = """
-        INSERT INTO Ingredient (ingredient_name)
-        VALUES($1)
+        INSERT INTO ingredients (name,user_id)
+        VALUES($1, $2)
         RETURNING id, name, created_at;
     """
 
-    return await pool.fetchrow(query, ingredient.name)
+    return await pool.fetchrow(query, name, user_id)
 
 async def ingredient_update(ingredient: IngredientUpdate) -> IngredientOut:
     """update ingredient """
