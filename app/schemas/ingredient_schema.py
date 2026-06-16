43f90eb84c22c50febcd55a25ddc066140626b21
dev_vens <vensalex1991@gmail.com>
2026-06-16 10:13:55 +0200

    Add doc on class.
    Add user_id data returned.
    
    Correction value name

--- app/schemas/ingredient_schema.py
+++ app/schemas/ingredient_schema.py
@@ -3,16 +3,18 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 class IngredientCreate(BaseModel):
+    """Data required to create a new ingredient (input)."""
     name: str
 
 class IngredientUpdate(BaseModel):
+    """Data allowed when updating an ingredient (input)."""
     id: int
     name: str
 
-class IngredientDelete(BaseModel):
-    id: int
 
 class IngredientOut(BaseModel):
+    """Data returned to the client (output)."""
     id: int
     name: str
-    creation_date: datetime
+    user_id: int | None
+    created_at: datetime
